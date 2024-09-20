18b072f9172cd097993de2a7d4b5b4f86b531b0a
Alexandre Lindsey <joueur.galactic@gmail.com>
2024-09-20 23:35:10 +0200

    Updated choose()

--- src/syscall.ts
+++ src/syscall.ts
@@ -111,7 +111,7 @@ export default {
                     }
                 }
             }
-            return [new ListValue(list), new StringValue(prompt), new BooleanValue(canMultiple), new BooleanValue(allSelected)]
+            return [new StringValue(list), new StringValue(prompt), new BooleanValue(canMultiple), new BooleanValue(allSelected)]
         }
     }
 }
